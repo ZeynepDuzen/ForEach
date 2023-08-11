e266a7ecf52421279760d3bf56b63bf358c10300
ZeynepDuzen <zeynepduzen59@gmail.com>
2023-08-11 09:52:31 +0300

    Add project files.

---
+++ ForEach/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForEach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] sorular = new string[]
+            {
+                "Kiskanc misin ? (1:hayir,2:evet)",
+                "Ask mi para mi? (1:ask, 2:para)",
+                "Kopek mi kedi mi ? (1:kopek,2:kedi)",
+                "Marka takintin var mi? (1:hayir,2:evet)",
+                "Zeka mi tip mi? (1:zeka 2: tip)"
+            };
+
+            string[] cevaplar = new string[sorular.Length];
+            for (int i = 0; i < sorular.Length; i++)
+            {
+                Console.WriteLine(sorular[i] + " ");
+                cevaplar[i] = Console.ReadLine();
+            }
+
+            int sonuc = 0;
+            foreach (string cevap in cevaplar)
+            {
+                if (cevap == "1")
+                {
+                    sonuc += 20;
+                }
+            }
+            if (sonuc > 70)
+            {
+                Console.WriteLine("Harik abir iliskiniz olabilir");
+            }
+            else if (sonuc >= 30 && sonuc <= 70)
+            {
+                Console.WriteLine("İliskiye bir sans verebilirsin");
+            }
+            else
+            {
+                Console.WriteLine("Bu iliskiden vazgecsen iyi olur");
+            }
+
+
+            Console.Read();
+        }
+    }
+}
